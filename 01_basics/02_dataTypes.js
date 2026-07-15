0f8048eb2da74d146a9d9319a6435e59c7d53ccb
Ayush Kumar <ayushff1515@gmail.com>
2026-07-15 11:30:35 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(9+4) // we are using nodejs, not browser
+
+console.log(3+3); console.log("Ayush")
+
+let name = "Ayush"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to the power of 53
+// bigint => larger than 2 to the power of 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique and cannot be changed
+
+// object => {key: value} => array, date, literal objects
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof age)
