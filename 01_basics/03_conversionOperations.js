d7dece160df504aa1dac3ee2d72150425a028673
Ayush Kumar <ayushff1515@gmail.com>
2026-07-16 06:48:16 +0000

    conversion of data types

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,3 +23,40 @@ let somenumber = 33
 let stringNumber = String(somenumber);
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// ************************** Operations ***************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "Ayushx"
+
+let str3 = str1 + str2 
+// console.log(str3)
+
+// console.log("2" + 2)
+// console.log(1+"2")
+// console.log("1"+ 2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4)*5%3)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter)
+
