1f69b4b2f1f5f959d1f991566cf3079948d17c2c
Ayush Kumar <ayushff1515@gmail.com>
2026-07-16 06:21:23 +0000

    conversion of datat ttypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = null
+
+console.log(typeof score)
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+
+// "33" => 33 
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false;
+// "" => false;
+// "ayush" => true;
+
+let somenumber = 33
+let stringNumber = String(somenumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
