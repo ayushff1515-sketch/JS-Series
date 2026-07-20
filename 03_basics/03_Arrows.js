f6b4930a9f9a623ce917ed8874e06d2647fdf4cf
Ayush Kumar <ayushff1515@gmail.com>
2026-07-20 06:58:05 +0000

    this and arrow

---
+++ 03_basics/03_Arrows.js
@@ -0,0 +1,50 @@
+const user = {
+    username: "Ayushx",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`)
+        console.log(this)
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "Aman"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// function chai(){
+//     let username= "Ayush"
+//     console.log(this.username)
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = "Ayush"
+//     console.log(this.username)
+// }
+
+const chai = () => {
+    let username = "Ayush"
+    console.log(this)
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1+num2
+
+// const addTwo = (num1, num2) => (num1+num2)
+
+const addTwo = (num1, num2) => ({username : "Ayush"})
+
+console.log(addTwo(3,4))
+
+// const myArr = [2,5,3,7,8]
+// myArr.forEach()
