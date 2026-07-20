85013b131b06791a2ccf7a7c0f8e9413913e70da
Ayush Kumar <ayushff1515@gmail.com>
2026-07-20 09:31:10 +0000

    control flow code files

---
+++ 04_control_flow/one.js
@@ -0,0 +1,56 @@
+// if 
+
+const isUserLoggedIn = true
+const temprature = 55
+
+// if (3!=2){
+//     console.log("EXECUTED")
+// }
+// if (2==="2"){
+//     console.log("EXECUTED")
+// }
+
+if(temprature <= 50){
+    console.log("Temperature is less then 50")
+}else{
+    console.log("Temperature is greater than 50")
+}
+
+// <, >, <=, >=, ==, !=
+
+const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User power: $(power)`)
+// }
+
+// console.log(`User power : $(power)`)
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2")
+
+// if (balance < 500){
+//     console.log("Leas than")
+// }else if(balance < 750){
+//     console.log("Less than 750")
+// }else if(balance , 900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1200")
+// }
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitcard && 2==3 ){
+    console.log("Allow to buy course")
+}
+
+if (userLoggedIn || loggedInFromEmail)[
+    console.log("User logged in")
+]
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,42 @@
+const userEmail = []
+
+if(userEmail){
+    console.log("Got user email")
+}else{
+    console.log("Don't have user email")
+}
+
+// falsy values 
+// false, 0, -0, bigint 0n, "", null, undefined, Nan
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty")
+// }
+
+const emptyobj={}
+
+if (Object.keys(emptyobj).length === 0){
+    console.log("Object is empty")
+}
+
+// Nullish coalescing Operator (??): null undefined
+
+let value
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1)
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >=80 ? console.log("Less than 80 ") : console.log("Greater than 80")
