9e2052ecf382d95a995e56ad7335cf363e12dbee
Ayush Kumar <ayushff1515@gmail.com>
2026-07-21 08:37:27 +0000

    reduce

---
+++ 05_Iterations/08_eight.js
@@ -0,0 +1,28 @@
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function(acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`)
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce((acc,curr) => acc+curr,0)
+
+console.log(myTotal)
+
+const shoppingCart =[ 
+    {
+        itemname: "js course",
+        price: 2999
+    },
+    {
+        itemname: "mobile dev course",
+        price: 999
+    },
+    {
+        itemname: "data science course",
+        price: 3999
+    }
+]
+const priceToPay = shoppingCart.reduce((acc,item) => acc + item.price,0)
+
+console.log(priceToPay)
